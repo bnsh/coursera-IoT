099e5cb6637032b2384d69beb65569a08b0bca6f
Binesh Bannerjee <binesh_binesh@hotmail.com>
2018-10-06 23:46:59 -0400

    This is the initial checkin.

---
+++ class2/week2/fib.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int fib[10];
+
+	fib[0] = 0;
+	fib[1] = 1;
+
+	for (int i = 2; i < 10; ++i) fib[i] = fib[i-1] + fib[i-2];
+	for (int i = 0; i < 6; ++i) {
+		printf("%d\n", fib[i]);
+	}
+
+	return(0);
+}
